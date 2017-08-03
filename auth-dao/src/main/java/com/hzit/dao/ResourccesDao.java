4651c380991f96b507a97c80a8c840d4e94e4159
1234567cao <fujiaxing00>
2017-08-03 17:34:05 +0800

    资源信息表

---
+++ auth-dao/src/main/java/com/hzit/dao/ResourccesDao.java
@@ -0,0 +1,20 @@
+package com.hzit.dao;
+
+import com.hzit.entity.Resourcces;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by THINK on 2017/8/3.
+ */
+public interface ResourccesDao {
+    public  List<Resourcces>  findAll ();
+    public  Resourcces  findRid (String rid);
+    public  Resourcces findRname (String rname);
+    public  boolean insert
+            (int rid,String rname,String resurl,  int resorderno,String redsdes,Date createTime,
+             Date updatetime,String createMan,String updateMan, int parentID);
+    public  boolean  delete(int rid);
+    public  boolean  update(int rid);
+}
